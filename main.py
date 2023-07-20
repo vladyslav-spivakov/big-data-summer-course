4eb589cd32b051a2e273faafffcfd80e23deaea4
Vladyslav <vladyslav.spivakov@gmail.com>
2023-07-20 14:49:05 +0300

    Added imports and usages of imdb_utils to main.py

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ spark = (SparkSession.builder
          .config(conf=SparkConf())
          .getOrCreate())
 
-# Schemas
+import imdb_utils.columns as c
 
 TITLE_BASICS_SCHEMA = t.StructType([
-    t.StructField("tconst", t.StringType(), nullable=False),
-    t.StructField("titleType", t.StringType()),
-    t.StructField("primaryTitle", t.StringType()),
-    t.StructField("originalTitle", t.StringType()),
-    t.StructField("isAdult", t.BooleanType()),
-    t.StructField("startYear", t.DateType()),
-    t.StructField("endYear", t.DateType()),
-    t.StructField("runtimeMinutes", t.StringType()),
-    t.StructField("genres", t.StringType()) # Array type not supported in csv
+    t.StructField(c.TCONST, t.StringType(), nullable=False),
+    t.StructField(c.TITLE_TYPE, t.StringType()),
+    t.StructField(c.PRIMARY_TITLE, t.StringType()),
+    t.StructField(c.ORIGINAL_TITLE, t.StringType()),
+    t.StructField(c.IS_ADULT, t.BooleanType()),
+    t.StructField(c.START_YEAR, t.DateType()),
+    t.StructField(c.END_YEAR, t.DateType()),
+    t.StructField(c.RUNTIME_MINUTES, t.StringType()),
+    t.StructField(c.GENRES, t.StringType())  # Array
 ])
 
 
