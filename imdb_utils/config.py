505e5b84778a74cfe8db511107cb29f6be69ce40
Vladyslav Spivakov <48495272+vladyslav-spivakov@users.noreply.github.com>
2023-07-20 14:52:08 +0300

    Revert "Imdb utils"

--- imdb_utils/config.py
+++
@@ -1,6 +0,0 @@
-SEP = "\t"
-TITLE_CREW_DATASET_PATH = ".\\datasets\\title.crew.tsv.gz"
-TITLE_BASICS_DATASET_PATH = ".\\datasets\\title.basics.tsv.gz"
-
-APP_NAME = 'my_assignment'
-MASTER = 'local'
